7bf1b4627cf789707893839a47ae3659fc43297f
Naresh Naik <126505787+Nareshnik@users.noreply.github.com>
2026-01-05 12:47:05 +0530

    Fix registration of Students Module

--- home/admin.py
+++ home/admin.py
@@ -3,5 +3,5 @@ from home.models import Permissions, Student, Faculty
 
 # Register your models here.
 admin.site.register(Permissions) 
-admin.site.register(Student) 
+admin.site.register(Students) 
 admin.site.register(Faculty)
